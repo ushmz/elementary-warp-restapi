ee73cccfef1fb1e84242298e44ac87de30bf491b
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-10-23 23:56:23 +0900

    :seedling: Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let greeting = warp::path!("hello" / String)
+        .map(|name: String| format!("Keep greeting to world forever, {}!", name));
+    warp::serve(greeting)
+        .run(([127, 0, 0, 1], 8000))
+        .await;
 }
